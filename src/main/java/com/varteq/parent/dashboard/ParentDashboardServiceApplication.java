b37d7006a61a40c8266ec473bc75927746d106e3
vshuliak <Vshuliak@varteq.com>
2020-03-31 14:51:23 +0300

    Initial Commit

---
+++ src/main/java/com/varteq/parent/dashboard/ParentDashboardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.varteq.parent.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParentDashboardServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParentDashboardServiceApplication.class, args);
+    }
+}
+
